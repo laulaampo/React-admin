023e602d006937481b84ccb58199600989ccbaa8
laulaampo <827274214@qq.com>
2020-01-12 16:52:07 +0800

    完成了登录组件的静态组件实习TRUE

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -3,7 +3,8 @@ import logo from './logo.png';
 import { Form, Icon, Input, Button } from 'antd';
 import './index.less'
 
-export default class Login extends Component {
+// @Form.create(Login)
+class Login extends Component {
   render() {
     return (
       <div className='login'>
@@ -12,25 +13,31 @@ export default class Login extends Component {
           <h1>硅谷后台管理系统</h1>
         </header>
         <section className="login-section">
-        <h2>用户登录</h2>
-        <Form >
-        <Form.Item className='login-form'>
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder="请输入用户名"
-            />
-            <Input
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-              type="password"
-              placeholder="请输入密码"
-            />
-            <Button type="primary" htmlType="submit" className="login-form-button">
-            登录
+          <h2>用户登录</h2>
+          <Form >
+            <Form.Item className='login-form'>
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                placeholder="请输入用户名"
+              />
+            </Form.Item>
+            <Form.Item className='login-form'>
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                placeholder="请输入密码"
+              />
+            </Form.Item>
+            <Form.Item className='login-form'>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                登录
           </Button>
-          </Form.Item>
-        </Form>
+            </Form.Item>
+          </Form>
         </section>
       </div>
     )
   }
 }
+
+export default Login;
